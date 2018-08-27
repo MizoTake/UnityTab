63824ae8f9a39779a135ede95d39fe9a911dee66
MizoTake <mizotake123@gmail.com>
2018-08-27 23:02:49 +0900

    add xamarin project

---
+++ UnityTabFromXamarin/UnityTabFromXamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UnityTabFromXamarin.Models;
+
+namespace UnityTabFromXamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
